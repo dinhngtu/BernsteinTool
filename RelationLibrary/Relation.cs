905ce927ec7e86de77471afb908168179b34119c
Dinh Ngoc Tu <none@none>
2014-12-02 11:21:06 +0700

    relation

--- RelationLibrary/Relation.cs
+++ RelationLibrary/Relation.cs
@@ -15,9 +15,9 @@ namespace RelationLibrary {
             this.FDs = fds;
         }
 
-        public static HashSet<Attribute> GetClosure(HashSet<FunctionalDependency> fds, HashSet<Attribute> determinants) {
+        public HashSet<Attribute> GetClosure(HashSet<Attribute> determinants) {
             var ret = new HashSet<Attribute>(determinants);
-            var testfds = new HashSet<FunctionalDependency>(fds);
+            var testfds = new HashSet<FunctionalDependency>(FDs);
             bool found;
             do {
                 found = false;
@@ -33,10 +33,6 @@ namespace RelationLibrary {
                 testfds.ExceptWith(fdToRemove);
             } while (found);
             return ret;
-        }
-
-        public HashSet<Attribute> GetClosure(HashSet<Attribute> determinants) {
-            return GetClosure(this.FDs, determinants);
         }
 
         public FunctionalDependency EliminateExtraneousAttributes(FunctionalDependency fd) {
@@ -63,23 +59,64 @@ namespace RelationLibrary {
         }
 
         public HashSet<FunctionalDependency> GetMinimalCovering() {
-            var fds = new HashSet<FunctionalDependency>(FDs);
+            var testfds = new HashSet<FunctionalDependency>(FDs);
             bool found;
             do {
                 found = false;
                 var fdToRemove = new HashSet<FunctionalDependency>();
-                foreach (var fd in fds) {
-                    var refClosure = GetClosure(fd.Determinants);
-                    var testfds = fds.GetExcepted(fd);
-                    if (refClosure.Contains(fd.Dependent)) {
+                foreach (var fd in testfds) {
+                    var excluded = testfds.GetExcepted(fd);
+                    var relExcluded = new Relation(this.Attributes, excluded);
+                    var closure = relExcluded.GetClosure(fd.Determinants);
+                    if (closure.Contains(fd.Dependent)) {
                         found = true;
                         fdToRemove.Add(fd);
                         break;
                     }
                 }
-                fds.ExceptWith(fdToRemove);
+                testfds.ExceptWith(fdToRemove);
             } while (found);
+            return testfds;
+        }
+
+        public static HashSet<Attribute> GetAttributes(IEnumerable<FunctionalDependency> fds) {
+            var attrs = new HashSet<Attribute>();
+            foreach (var fd in fds) {
+                attrs.Add(fd.Dependent);
+                attrs.UnionWith(fd.Determinants);
+            }
+            return attrs;
+        }
+
+        public HashSet<Attribute> GetCandidateKey() {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<FunctionalDependency> GetFunctionalDependencies(HashSet<Attribute> attributes) {
+            var fds = new HashSet<FunctionalDependency>();
+            foreach (var fd in this.FDs) {
+                if (attributes.Contains(fd.Dependent) && attributes.IsSupersetOf(fd.Determinants)) {
+                    fds.Add(fd);
+                }
+            }
             return fds;
+        }
+
+        public HashSet<Relation> CreateRelations() {
+            var relations = new HashSet<Relation>();
+            var keygroups = FDs.GroupBy(fd => fd.Determinants);
+            foreach (var group in keygroups) {
+                relations.Add(new Relation(GetAttributes(group), new HashSet<FunctionalDependency>(group)));
+            }
+            var key = this.GetCandidateKey();
+            relations.Add(new Relation(key, GetFunctionalDependencies(key)));
+            var dedup = new HashSet<Relation>();
+            foreach (var rel in relations) {
+                if (!relations.Any(r => rel.Attributes.IsSubsetOf(r.Attributes))) {
+                    dedup.Add(rel);
+                }
+            }
+            return dedup;
         }
     }
 }
